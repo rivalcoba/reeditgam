9c5943f6bc5b5f0d4e8c735f3ec0a5f686616315
rivalcoba <ivan.rivalcoba@gmail.com>
2015-04-23 12:38:18 -0500

    06. Se agrega funcionalidad de compartir links y se carga boostrap con exito

--- site/scripts/app.js
+++ site/scripts/app.js
@@ -24,9 +24,16 @@ modulo1.controller("mainCtrl",[
 		 	$scope.posts.push(
 		 		{
 		 			title: $scope.title,
+		 			link: $scope.link,
 		 		 	upvotes: 0
 		 		 });
 		 	// Two-way data binding
 		 	$scope.title = "";
+		 	$scope.link = "";
+		 };
+		 // Metodo que incrementa el voto
+		 // de un post en una unidad
+		 $scope.incrementUpvotes = function(post){
+		 	post.upvotes += 1;
 		 };
 	}]);
